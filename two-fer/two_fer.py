aac4f1ea3dafc21c6616857329844c3a68b70534
Julia Guimaraes <36795273+taspsandiego@users.noreply.github.com>
2019-09-10 13:35:27 -0300

    Updated solution after feedback

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,5 +1,2 @@
-def two_fer(name=""):
-	if (name==""):
-		return "One for you, one for me."
-	else:
-		return "One for "+name+", one for me."
+def two_fer(name="you"):
+	return f'One for {name}, one for me.'
